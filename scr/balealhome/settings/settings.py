ce0c85a8b5730f3b62c558a8d024be0cd4536d27
Alex P <alponamor@gmail.com>
2017-11-24 17:25:18 +0300

    settings.py: INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS. Local server works.

--- scr/balealhome/settings/settings.py
+++ scr/balealhome/settings/settings.py
@@ -29,16 +29,21 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
-INSTALLED_APPS = [
+# Приложения разбиты на2 списка: первый - глобальные зависимости, второй - специфичные для данного проекта.
+PREREQ_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+]
+
+PROJECT_APPS = [
     'balealhome.cashflow',
 ]
+
+INSTALLED_APPS = PREREQ_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
